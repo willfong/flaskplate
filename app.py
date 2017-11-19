d647bfa9ae62f25432d60e577bd123b8b770b1b2
Will Fong <w@r10e.xyz>
2017-11-19 20:20:54 +0800

    Updates

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 import sqlite3
+import hashlib
 import string
 import random
+from functools import wraps
 from flask import Flask, session, redirect, url_for, render_template, flash, g, request
 
 app = Flask(__name__)
@@ -31,19 +33,23 @@ def read_db(query, args=(), one=False):
         # This one logs everything.
         print "Database Error: {}".format(err)
 
-def write_db(query, args=()):
+def write_db(query, params=(), last_id=False):
     if isinstance(params, tuple):
         args = params
     else:
         args = (params,)
     try:
-        get_db().cursor().execute(query, args)
+        cur = get_db().cursor()
+        cur.execute(query, args)
         get_db().commit()
     except sqlite3.Error as err:
-        # TODO: need a way to determine operational error vs unique error
-        # Don't log operational errors like unique key violations
-        # This one logs everything.
+        # TODO: need a way to determine operational error
         print "Database Error: {}".format(err)
+        return False
+    if last_id:
+        return cur.lastrowid
+    else:
+        return True
 
 def init_db():
     with app.app_context():
@@ -54,6 +60,15 @@ def init_db():
         print "Testing initial users..."
         query = ("SELECT * FROM users")
         print read_db(query)
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not 'user_id' in session:
+            flash('Please login first', 'warning')
+            return redirect(url_for('index'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -68,26 +83,24 @@ def index():
     return render_template('index.html')
 
 @app.route('/main')
+@login_required
 def main():
-    if not 'user_id' in session:
-        return redirect(url_for('login'))
     query = ("SELECT id, username FROM users")
     users = read_db(query)
     return render_template('main.html', users=users)
 
 @app.route('/settings', methods=['GET','POST'])
+@login_required
 def settings():
-    if not 'user_id' in session:
-        return redirect(url_for('login'))
     if request.method == 'POST':
         if request.form['action'] == 'changepassword':
             query = ("UPDATE users SET password = ? WHERE id = ?")
-            params = (request.form['password'], session['user_id'])
+            params = (hashlib.sha256(request.form['password']).hexdigest()), session['user_id'])
             write_db(query, params)
             flash('Successfully updated password', 'success')
         elif request.form['action'] == 'createuser':
             query = ("INSERT INTO users (username, password) VALUES (?, ?)")
-            params = (request.form['username'], request.form['password'])
+            params = (request.form['username'], hashlib.sha256(request.form['password']).hexdigest()))
             write_db(query, params)
             flash('Successfully added user', 'success')
     return render_template('settings.html')
@@ -96,7 +109,7 @@ def settings():
 def login():
     if request.method == 'POST':
         query = ("SELECT id FROM users WHERE username = ? AND password = ?")
-        params = (request.form['username'], request.form['password'])
+        params = (request.form['username'], hashlib.sha256(request.form['password']).hexdigest()))
         user = read_db(query, params, one=True)
         if user is None:
             flash('Username/Password not found!', 'danger')
